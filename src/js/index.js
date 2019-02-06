301b9a16e14fb0e8d6d1bfadcc6e41f4d85548eb
TeonMehta <teonmehta@icloud.com>
2019-02-06 13:35:32 -0500

    HOME PAGE DONE

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,32 @@
 import '../styles/style.scss';
 import 'bootstrap'
 import 'popper.js'
+import TypeIt from 'typeit';
+
+
+new TypeIt('#billion', {
+    speed: 60,
+    breakLines: false,
+    waitUntilVisible: true
+})
+    .type("A million dollars isn't cool.")
+    .pause(1500)
+    .options({speed: 80})
+    .delete()
+    .pause(500)
+    .options({speed: 40})
+    .type("You know what's cool?")
+    .pause(2500)
+    .options({speed: 80})
+    .delete()
+    .options({speed: 50})
+    .type("<strong>A BILLION DOLLARS.</strong>")
+    .go();
+
+
+// function myFunction() {
+//     document.body.style.backgroundColor = "#1b1b1b";
+//     document.getElementById('billion').style.color = "red";
+// }
+//
+// myFunction();
